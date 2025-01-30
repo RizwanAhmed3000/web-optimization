81d8955eb2f950d315ee9843e5a3858a1cefefe0
Rizwan Ahmed <121274660+RizwanAhmed3000@users.noreply.github.com>
2025-01-30 20:13:13 +0500

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,19 +10,19 @@ const Home = () => {
 
   // FETCH METHOD FOR GETTING DATA
 
-  //   useEffect(() => {
-  //     const fetchData = async () => {
-  //       try {
-  //         const response = await fetch("https://api.example.com/data");
-  //         const result = await response.json();
-  //         setData(result);
-  //       } catch (error) {
-  //         console.error("Error fetching data:", error);
-  //       }
-  //     };
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+          const response = await fetch("https://api.example.com/data");
+          const result = await response.json();
+          setData(result);
+        } catch (error) {
+          console.error("Error fetching data:", error);
+        }
+      };
 
-  //     fetchData();
-  //   }, []);
+      fetchData();
+    }, []);
 
   // AXIOS METHOD GETTING FOR DATA
 
